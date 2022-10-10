cafe052ba34d9f08fd0af676b446cf1e44f2dce0
DESKTOP-SBMED09\frog <sweetpoteto@naver.com>
2022-10-11 02:12:48 +0900

    템플릿 상속 완료

--- IllustNet/views/main_views.py
+++ IllustNet/views/main_views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return render(request, 'main_page.html')
+	return render(request, 'illust_board.html')
